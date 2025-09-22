e6892396d1661bfea5e11c76fe855d3d810976d9
Afsar Tanvir <afsartanvir805@gmail.com>
2025-09-22 15:38:46 +0600

    State and Handling Events using React

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
-    <Navbar title="Afsar Tanvir" />
+    <>
+      <Navbar title="Afsar Tanvir" />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below" />
+      </div>
+    </>
   );
 }
 
