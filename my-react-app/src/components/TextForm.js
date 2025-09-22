f9422390a6d90e290b0e1b7d4bdab3dc0d274512
Afsar Tanvir <afsartanvir805@gmail.com>
2025-09-22 17:43:21 +0600

    Adding New Feature Removing Extra Space, Copy Text to the Clipboard

--- my-react-app/src/components/TextForm.js
+++ my-react-app/src/components/TextForm.js
@@ -1,30 +1,46 @@
 import React, { useState } from "react";
 
 export default function TextForm(parameters) {
+  // Convert To Uppercase
   const handleUpClick = () => {
     console.log("uppercase was clicked: " + text);
     let newText = text.toUpperCase();
     setText(newText);
   };
 
+  // Convert To Lowercase
   const handleLoClick = () => {
     console.log("lowercase was clicked: " + text);
     let newText = text.toLowerCase();
     setText(newText);
   };
 
+  // Clear The Text
   const handleClearClick = () => {
-    setText('');
+    setText("");
   };
 
+  // On Change
   const handleOnChange = (event) => {
     console.log("On change");
     setText(event.target.value);
   };
 
+  // Copy Text
+  const handleCopyText = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    text.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(text.value);
+  };
+
+  // Removing Extra Space
+  const handleExtraSpace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
   const [text, setText] = useState("");
-  // text = "new text"; // Wrong way to change the state
-  // setText("new text"); // Correct way to change the state
   return (
     <>
       <div>
@@ -45,7 +61,13 @@ export default function TextForm(parameters) {
           Convert to Lowercase
         </button>
         <button className="btn btn-primary mx-1" onClick={handleClearClick}>
-          Clear text
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopyText}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>
+          Remove Extra Spaces
         </button>
       </div>
       <div className="container my-3">
