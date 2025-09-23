6529b623c9ae961a8f52a541f1fadc24c04598ce
Afsar Tanvir <afsartanvir805@gmail.com>
2025-09-23 13:28:56 +0600

    React Router Setup, Usage

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,15 +4,23 @@ import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
+      // setInterval(() => {
+      //   document.title = "TextUtils is Amazing Mode";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Install TextUtils Now";
+      // }, 1500);
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
@@ -20,7 +28,6 @@ function App() {
     }
   };
 
-  const [alert, setAlert] = useState(null);
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
@@ -34,16 +41,25 @@ function App() {
 
   return (
     <>
-      <Navbar title="Afsar Tanvir" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm
-          heading="Enter the text to analyze below"
-          mode={mode}
-          showAlert={showAlert}
-        />
-      </div>
-      <About />
+      <Router>
+        <Navbar title="Afsar Tanvir" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter the text to analyze below"
+                  mode={mode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- my-react-app/src/components/Navbar.js
+++ my-react-app/src/components/Navbar.js
@@ -1,14 +1,19 @@
+import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-export default function Navbar(props) {
+export default function Navbar({
+  title = "Set title here",
+  aboutText = "About",
+  mode,
+  toggleMode,
+}) {
   return (
-    <nav
-      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
-    >
+    <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          {props.title}
-        </a>
+        <Link className="navbar-brand" to="/">
+          {title}
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -23,40 +28,40 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
+              <Link className="nav-link" to="/about">
+                {aboutText}
+              </Link>
             </li>
             <li className="nav-item dropdown">
-              <a
+              <Link
                 className="nav-link dropdown-toggle"
-                href="#"
+                to="/"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
                 Dropdown
-              </a>
+              </Link>
               <ul className="dropdown-menu">
                 <li>
-                  <a className="dropdown-item" href="/">
+                  <Link className="dropdown-item" to="/">
                     Action
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="/">
+                  <Link className="dropdown-item" to="/">
                     Another action
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="/">
+                  <Link className="dropdown-item" to="/">
                     Something else here
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </li>
@@ -79,12 +84,12 @@ export default function Navbar(props) {
           </form>
           <div
             className={`form-check form-switch text-${
-              props.mode === "light" ? "dark" : "light"
+              mode === "light" ? "dark" : "light"
             }`}
           >
             <input
               className="form-check-input mx-1"
-              onClick={props.toggleMode}
+              onClick={toggleMode}
               type="checkbox"
               id="flexSwitchCheckDefault"
             />
@@ -101,13 +106,3 @@ export default function Navbar(props) {
     </nav>
   );
 }
-
-Navbar.prototype = {
-  title: PropTypes.string.isRequired,
-  aboutText: PropTypes.string.isRequired,
-};
-
-Navbar.defaultProps = {
-  title: "Set title here",
-  aboutText: "About",
-};
